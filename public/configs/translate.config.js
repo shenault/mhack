10f6d1648649c01ff94daef184abb82a7040b086
shenault <shenault@omnimed.com>
2016-04-23 11:06:07 -0400

    Menu avec griselage de la vue en cours + i18n

--- public/configs/translate.config.js
+++ public/configs/translate.config.js
@@ -7,9 +7,19 @@
 				var locale = navigator.languages ? navigator.languages[0] : navigator.language;
 
 				$translateProvider.translations('en', {
+					close : 'Close',
+					menu : 'Menu',
+					'menu.eventReporter' : 'Report an event',
+					'menu.eventManager' : 'Resolve an event',
+					'menu.groupManager' : 'Manage groups',
 					title: 'Hello'
 				});
 				$translateProvider.translations('fr', {
+					close : 'Fermer',
+					menu : 'Menu',
+					'menu.eventReporter' : 'Rapporter un événement',
+					'menu.eventManager' : 'Résoudre un événement',
+					'menu.groupManager' : 'Gérer les groupes',
 					title: 'Allo'
 				});
 
--- public/controllers/menu.ctrl.js
+++ public/controllers/menu.ctrl.js
@@ -10,17 +10,19 @@
 			'$state',
 			function($scope, $state) {
 				$scope.goToState = function(state) {
+					$scope.state = state;
 					$state.go(state);
 					$scope.menuOpened = false;
-				}
+				};
 
 				$scope.setupScope = function() {
+					$scope.state = 'eventReporter';
 					$scope.menuOpened = false;
 				};
 
 				$scope.toggleMenu = function() {
 					$scope.menuOpened = !$scope.menuOpened;
-				}
+				};
 
 				$scope.setupScope();
 			}]);

b7aad36504eedd96314d78607dbe61547764dd73
glafortune <glafortune@omnimed.com>
2016-04-23 16:47:38 -0400

    GetTYpe for position

--- public/utils/google.utils.js
+++ public/utils/google.utils.js
@@ -22,13 +22,71 @@
 						var refGroup = new Firebase("https://mhack-ghislain.firebaseio.com/group");
 						var self = this;
 						var returnValue = [];
+						var returnValueKey = [];
 
 						refGroup.orderByKey().on("value", function(snapshot) {
 							snapshot.forEach(function(data) {
+								angular.forEach(data.val().zone,function(zone, key){
+									var refZone = new Firebase("https://mhack-ghislain.firebaseio.com/zone");
+									refZone.orderByKey().equalTo(zone).on("value", function(snapshot){
+										snapshot.forEach(function(zone) {
+											console.log(data.val().name + " " +  zone.val().desc + " " + zone.val().path.length);
+											var amIinZone = false;
+											for (var i=0; i < zone.val().path.length; i++ ) {
+												if (zone.val().path[i].type = "polygone") {
+													//create polygone
+													var paths = [];
 
+													for (var j=0; j< zone.val().path[i].coordinates.length;j++) {
+														var lat = zone.val().path[i].coordinates[j][0];
+														var lng = zone.val().path[i].coordinates[j][1];
+
+														paths.push(new google.maps.LatLng(lat, lng));
+													}
+
+													var polygon = new google.maps.Polygon({paths});
+													if (self.isInPolygon(lat, lng,polygon)) {
+														amIinZone = true;
+														break;
+													}
+												} else if (zone.val().path[i].type = "LineString") {
+													//creer la structure
+													//comparer avec le treshold
+													if (true) {
+														amIinZone = true;
+														break;
+													}
+												}
+											};
+
+											if (amIinZone) {
+												//return all type of the group
+												for (var i=0; i < data.val().type.length; i++){
+													console.log(data.val().type[i]);
+													returnValueKey.push(data.val().type[i]);
+												}
+
+											}
+
+										});
+									});
+								});
 							});
 						});
-						return returnValue;
+
+						//get unique
+						returnValueKey =  returnValueKey.filter(function(item, i, ar){ return ar.indexOf(item) === i; });
+
+						//get type
+						var refType = new Firebase("https://mhack-ghislain.firebaseio.com/type");
+
+						for (var i = 0; i<returnValueKey.length;i++) {
+							refType.orderByKey().equalTo(returnValueKey[i]).on("value", function (snapshot) {
+								returnValue.push(snapshot.val());
+							});
+						}
+						console.log(returnValue);
+						return returnValue
 					},
 
 					isInPolygon: function(lat, lng, p) {

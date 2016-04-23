b734a202db479391e00126104ea0c29a09bfe474
glafortune <glafortune@omnimed.com>
2016-04-23 14:08:08 -0400

    Ajout de funtion

--- public/utils/google.utils.js
+++ public/utils/google.utils.js
@@ -14,11 +14,20 @@
 			'$rootScope',
 			'$translate',
 			'$window',
-			function(MedicationDosageUtils, MedicationPrescriptionLinkUtils, TermUtils, $filter, $rootScope, $translate, $window) {
+			'$firebaseArray',
+			function( $filter, $rootScope, $translate, $window,$firebaseArray) {
 				return {
 
 					getTypeList: function(lat, lng) {
+						var refGroup = new Firebase("https://mhack-ghislain.firebaseio.com/group");
+						var self = this;
+						var returnValue = [];
 
+						refGroup.orderByKey().on("value", function(snapshot) {
+							snapshot.forEach(function(data) {
+
+							});
+						});
 						return returnValue;
 					},
 
@@ -33,11 +42,41 @@
 					},
 
 					getItemListNearMe: function(lat, lng, meters) {
+						var ref = new Firebase("https://mhack-ghislain.firebaseio.com/item");
+						var self = this;
+						var returnValue = [];
 
+						ref.orderByKey().on("value", function(snapshot) {
+							snapshot.forEach(function(data) {
+								if (meters >= self.getDistanceFromLatLonInMeter(lat,lng, data.val().lat,data.val().lng)) {
+									returnValue.push(data.val());
+								}
+							});
+						});
+						return returnValue;
 					},
 
 					getItemListInViewPort: function(minLat, minLng, maxLat, maxLng) {
 
+					},
+
+					getDistanceFromLatLonInMeter: function(lat1,lon1,lat2,lon2) {
+						var self = this;
+						var R = 6371; // Radius of the earth in km
+						var dLat = self.deg2rad(lat2-lat1);  // deg2rad below
+						var dLon = self.deg2rad(lon2-lon1);
+						var a =
+								Math.sin(dLat/2) * Math.sin(dLat/2) +
+								Math.cos(self.deg2rad(lat1)) * Math.cos(self.deg2rad(lat2)) *
+								Math.sin(dLon/2) * Math.sin(dLon/2)
+							;
+						var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
+						var d = R * c; // Distance in km
+						return d * 1000;
+					},
+
+					deg2rad : function(deg) {
+						return deg * (Math.PI/180)
 					}
 
 				};

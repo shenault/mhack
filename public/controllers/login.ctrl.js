8afff5d71de3ce330dcd00551bef9033ae39456d
nmichaud <nmichaud@omnimed.com>
2016-04-23 13:37:27 -0400

    auth login suite

--- public/controllers/login.ctrl.js
+++ public/controllers/login.ctrl.js
@@ -7,11 +7,21 @@
 
 		.controller('LoginController', [
 			'$auth',
+			'$http',
 			'$scope',
 			'$state',
-			function($auth, $scope, $state) {
+			function($auth, $http, $scope, $state) {
 				$scope.authenticate = function(provider) {
 					$auth.authenticate(provider).then(function(accessToken) {
+						$http({
+							method: 'GET',
+							url: 'https://www.googleapis.com/oauth2/v1/userinfo?access_token=' + accessToken
+						}).then(function successCallback(response) {
+							console.log(response);
+						}, function errorCallback(response) {
+							// called asynchronously if an error occurs
+							// or server returns response with an error status.
+						});
 						$state.go('eventReporter');
 					})
 				};
